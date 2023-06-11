8b611b4cab3bff08ecda80a53da23626b5285f90
Jay <jayhome@smail.nchu.edu.tw>
2023-06-11 23:08:26 +0800

    add auto complete

--- brew.sh
+++ brew.sh
@@ -18,5 +18,6 @@ else
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 brew install --cask iterm2
 export ZSH_THEME="agnoster"
